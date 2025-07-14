5db6cbf474279da93fa0f189688f9ee1f4f99464
Tonyp85-25 <anthony.paulin@enil-mamirolle.fr>
2025-07-14 11:10:45 +0200

    feat: add sprite position management

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, inject, Input, input } from '@angular/core';
+import { CardsService } from '../cards.service';
 
 @Component({
   selector: 'app-card',
@@ -13,13 +14,16 @@ import { Component, Input, input } from '@angular/core';
 export class CardComponent {
   
   @Input({required: true}) data! :{fruit:string}
-
+  cardsService = inject(CardsService);
   isPlayed = false;
+
+  fruitPosition ="" ;
 
   onClick(event: MouseEvent) {
     console.log(event);
     event.stopPropagation();
     this.isPlayed = !this.isPlayed
+    this.fruitPosition=this.cardsService.getPosition(this.data.fruit)
   }
 
 }
--- src/app/cards.service.ts
+++ src/app/cards.service.ts
@@ -9,6 +9,8 @@ export class CardsService {
 
   private cards :  CardModel[] = [];
 
+  private readonly SPRITE_SPACE = 100;
+
   constructor() { 
     this.cards = fruits.map(fruit => ({fruit}));
   }
@@ -16,5 +18,10 @@ export class CardsService {
   getCards() {
     const slicedCards = this.cards.slice(0,14);
     return slicedCards.concat(slicedCards);
+  }
+
+  getPosition(fruit: string) {
+    const index =this.cards.findIndex(card => card.fruit === fruit);
+    return `0px ${-index * this.SPRITE_SPACE}px`;
   }
 }
