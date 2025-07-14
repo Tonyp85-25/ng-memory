68cc58142f5c662558d3aa892ef88bc83a8cfea1
Tonyp85-25 <anthony.paulin@enil-mamirolle.fr>
2025-07-14 14:04:01 +0200

    feat: add cards preparation

--- src/app/cards.service.ts
+++ src/app/cards.service.ts
@@ -3,23 +3,22 @@ import { CardModel } from './models/card.model';
 import { fruits } from './models/fruits';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CardsService {
-
-  private cards :  CardModel[] = [];
+  private cards: CardModel[] = [];
   private cardsStack: CardModel[] = [];
 
   private readonly SPRITE_SPACE = 100;
 
-  constructor() { 
-    this.cards = fruits.map(fruit => ({fruit}));
+  constructor() {
+    this.cards = fruits.map((fruit) => ({ fruit }));
     this.prepareCards();
   }
 
   private prepareCards() {
-     const slicedCards = this.cards.slice(0,14);
-      this.cardsStack = slicedCards.concat(slicedCards);
+    const slicedCards = this.cards.slice(0, 14);
+    this.cardsStack = slicedCards.concat(slicedCards);
   }
 
   getCards() {
@@ -28,14 +27,17 @@ export class CardsService {
   }
 
   getPosition(fruit: string) {
-    const index =this.cards.findIndex(card => card.fruit === fruit);
+    const index = this.cards.findIndex((card) => card.fruit === fruit);
     return `0px ${-index * this.SPRITE_SPACE}px`;
   }
 
-  private shuffle(){
-      for (let i = this.cardsStack.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		[this.cardsStack[i], this.cardsStack[j]] = [this.cardsStack[j], this.cardsStack[i]];
-	}
+  private shuffle() {
+    for (let i = this.cardsStack.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cardsStack[i], this.cardsStack[j]] = [
+        this.cardsStack[j],
+        this.cardsStack[i],
+      ];
+    }
   }
 }
